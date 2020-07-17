210dba5a5b0323f52e98f44d2554c5d7fdfc7375
Marcio Motta <mamotta@eldorado.org.br>
2020-07-17 16:37:50 -0300

    Add switch button and save its status using SharedPreferences

--- app/src/main/java/com/freemansys/movelight/MainActivity.java
+++ app/src/main/java/com/freemansys/movelight/MainActivity.java
@@ -1,14 +1,61 @@
 package com.freemansys.movelight;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private ImageButton powerButton;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor editor;
+    private boolean flashStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        powerButton = (ImageButton) findViewById(R.id.power_button);
+        powerButton.setOnClickListener(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = mSharedPreferences.edit();
+
+        flashStatus = mSharedPreferences.getBoolean("is_flashlight_enabled", false);
+
+        changePowerButtonColor(flashStatus);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.power_button:
+                changeFlashlightStatus();
+                break;
+        }
+    }
+
+    private void changePowerButtonColor(boolean isEnabled) {
+        int newColor = (isEnabled) ? Color.GREEN : Color.WHITE;
+        powerButton.setColorFilter(newColor);
+    }
+
+    private void changeFlashlightStatus(){
+        if(flashStatus == true){
+            editor.putBoolean("is_flashlight_enabled", false);
+        }else{
+            editor.putBoolean("is_flashlight_enabled", true);
+        }
+
+        editor.commit();
+        flashStatus = !flashStatus;
+        changePowerButtonColor(flashStatus);
     }
 }
