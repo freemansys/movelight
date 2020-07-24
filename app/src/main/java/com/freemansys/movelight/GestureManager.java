aafb5de5eb86f1943709e85ec89418d1af901bb9
Marcio Motta <marciovcmotta@hotmail.com>
2020-07-24 13:43:49 -0300

    Chop timeout and double chop event trigger were implemented

--- app/src/main/java/com/freemansys/movelight/GestureManager.java
+++ app/src/main/java/com/freemansys/movelight/GestureManager.java
@@ -5,17 +5,20 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 public class GestureManager implements SensorEventListener {
 
-    private static final int DELAY_BETWEEN_CHOPS_MS = 750;
-    private static final float THRESHOLD_TRIGGER_EVENT = 30f;
+    private static final int DELAY_BETWEEN_CHOPS_MS = 250;
+    private static final int CHOP_RESET_TIME_MS = 1000;
+    private static final float THRESHOLD_TRIGGER_EVENT = 25f;
+    private static final int CHOP_LIMIT = 2;
 
     private SensorManager msSensorManager;
     private Sensor mSensor;
 
     private long lastTimeChopDetected;
-    private boolean isGestureInProgress = false;
+    private int chopCount = 0;
 
     public interface ChopListener {
         void onChop();
@@ -36,17 +39,29 @@ public class GestureManager implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         float x = event.values[0];
-        long timeDelta = 0;
+        long timeNow = 0;
 
         if (Math.abs(x) > THRESHOLD_TRIGGER_EVENT) {
-            lastTimeChopDetected = System.currentTimeMillis();
-            isGestureInProgress = true;
-        } else {
-            timeDelta = System.currentTimeMillis() - lastTimeChopDetected;
-            if (timeDelta > DELAY_BETWEEN_CHOPS_MS && isGestureInProgress) {
-                isGestureInProgress = false;
-                chopListener.onChop();
+            timeNow = System.currentTimeMillis();
+            if (lastTimeChopDetected + DELAY_BETWEEN_CHOPS_MS > timeNow) {
+                return;
             }
+
+            if (lastTimeChopDetected != 0 && (timeNow - lastTimeChopDetected) > CHOP_RESET_TIME_MS) {
+                Log.d("Movelight", "chop timeout");
+                lastTimeChopDetected = 0;
+                chopCount = 0;
+                return;
+            }
+
+            lastTimeChopDetected = timeNow;
+            chopCount++;
+            Log.d("Movelight", "chopCount: " + chopCount);
+        }
+
+        if (chopCount >= CHOP_LIMIT) {
+            chopListener.onChop();
+            chopCount = 0;
         }
     }
 
